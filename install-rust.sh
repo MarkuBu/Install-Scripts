dc4cb3ee323598e1ddffd082ca64920a4862e37c
MarkuBu <rustasian@tuta.io>
2021-06-05 16:03:13 +0200

    Add cargo env and cargo-update

--- install-rust.sh
+++ install-rust.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 echo Install necessary dependencies
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl
 
 echo Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+source ~/.cargo/env
 
+echo Install Rust Analyzer
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.cargo/bin/rust-analyzer
+chmod +x ~/.cargo/bin/rust-analyzer
+
+cargo install cargo-update
