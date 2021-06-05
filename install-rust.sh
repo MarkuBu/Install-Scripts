9710e51e4b0c46c5fc70191cd4b20d1132ad3472
MarkuBu <rustasian@tuta.io>
2021-06-05 16:17:23 +0200

    Update install-rust.sh

--- install-rust.sh
+++ install-rust.sh
@@ -7,10 +7,9 @@ sudo apt install -y curl
 echo Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-source $HOME/.cargo/env
 
 echo Install Rust Analyzer
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.cargo/bin/rust-analyzer
 chmod +x ~/.cargo/bin/rust-analyzer
 
-cargo install cargo-update
+~/.cargo/bin/cargo install cargo-update
