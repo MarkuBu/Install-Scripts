65e6d99c32517d54a6485d67a4bcce17d60657ae
MarkuBu <rustasian@tuta.io>
2021-06-05 19:59:57 +0200

    Delete install-xorg.sh

--- install-xorg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Update system --------------------------------------------------------
-sudo apt update && sudo apt upgrade -y
-
-# This will install nodm for now. Will be improved later
-
-echo Install Xorg ---------------------------------------------------------
-sudo apt install xorg nodm
