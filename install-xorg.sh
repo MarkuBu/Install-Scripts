4e60df3371be6702374f966587caa8ae6932015d
MarkuBu <rustasian@tuta.io>
2021-06-05 17:11:08 +0200

    Initial commit

---
+++ install-xorg.sh
@@ -0,0 +1,11 @@
+#!/usr/bash
+
+clear
+
+echo Update system --------------------------------------------------------
+sudo apt update && sudo apt upgrade -y
+
+# This will install nodm for now. Will be improved later
+
+echo Install Xorg ---------------------------------------------------------
+sudo apt install xorg nodm
