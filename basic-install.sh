dc15f35c6451c196cb94a0de72f9a3876af48b3f
MarkuBu <rustasian@tuta.io>
2021-06-07 09:15:45 +0200

    Update basic-install

--- basic-install.sh
+++ basic-install.sh
@@ -2,15 +2,18 @@
 
 clear
 
+sudo apt update && sudo apt upgrade -y
+sudo apt install software-properties-common
+
 # neovim 0.5 is necessary. Unstable PPA will be replaced with stable PPA when 0.5 is released
 echo Add neovim unstable PPA
 sudo add-apt-repository ppa:neovim-ppa/unstable
 
 echo Update System
-sudo apt update && sudo apt upgrade -y
+sudo apt update 
 
 echo Basic Installation
-sudo apt install -y curl neovim upower acpi dialog software-properties-common xterm
+sudo apt install -y curl neovim upower acpi dialog xterm fzf terminator
 
 echo Install development stuff
 sudo apt install -y build-essential g++ pkg-config libssl-dev linux-headers-generic
