a737fe8fb8c33aa1c37e311b123b6aa1d0e8192a
Markus Burrer <markus@miniiso.fritz.box>
2021-06-08 19:21:55 +0200

    Add Xmonad

--- basic-install.sh
+++ basic-install.sh
@@ -7,13 +7,13 @@ sudo apt install software-properties-common
 
 # neovim 0.5 is necessary. Unstable PPA will be replaced with stable PPA when 0.5 is released
 echo Add neovim unstable PPA
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 
 echo Update System
-sudo apt update 
+sudo apt update -y 
 
 echo Basic Installation
-sudo apt install -y curl neovim upower acpi dialog xterm fzf terminator
+sudo apt install -y curl neovim upower acpi dialog xterm fzf 
 
 echo Install development stuff
 sudo apt install -y build-essential g++ pkg-config libssl-dev linux-headers-generic
