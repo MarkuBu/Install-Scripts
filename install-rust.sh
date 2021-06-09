aac46b2402a31e3aeba3a3f647e5f18a015ffab4
MarkuBu <rustasian@tuta.io>
2021-06-09 18:58:42 +0200

    Add Rust ARM dev tools

--- install-rust.sh
+++ install-rust.sh
@@ -4,6 +4,8 @@ echo Install necessary dependencies
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl
 
+sudo apt install gdb-multiarch openocd qemu-system-arm
+
 echo Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
@@ -13,3 +15,15 @@ curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/
 chmod +x ~/.cargo/bin/rust-analyzer
 
 ~/.cargo/bin/cargo install cargo-update
+
+rustup target add thumbv6m-none-eabi
+rustup target add thumbv7m-none-eabi
+rustup target add thumbv7em-none-eabi
+rustup target add thumbv7em-none-eabihf
+rustup target add thumbv8m.base-none-eabi
+rustup target add thumbv8m.main-none-eabi
+rustup target add thumbv8m.main-none-eabihf
+
+cargo install cargo-binutils
+rustup component add llvm-tools-preview
+cargo install cargo-generate
