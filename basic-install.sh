3e3450c03ce5eea5563d1653169810e5701db4d7
MarkuBu <rustasian@tuta.io>
2021-06-09 09:03:45 +0200

    libss-dev added

--- basic-install.sh
+++ basic-install.sh
@@ -16,7 +16,7 @@ echo Basic Installation
 sudo apt install -y curl neovim upower acpi dialog xterm fzf terminator
 
 echo Install development stuff
-sudo apt install -y build-essential g++ pkg-config libssl-dev linux-headers-generic
+sudo apt install -y build-essential g++ pkg-config libssl-dev linux-headers-generic libssl-dev
 
 echo Install Network Manager
 sudo apt --no-install-recommends install -y network-manager 
